3aa612e9a075575e9caf2186299c3cbbb1ebc3cc
Jay Cha <kimchikimchi@gmail.com>
2021-05-08 12:43:42 -0400

    - In HTML, instead of displaying a mock date in placeholder, change it to 'MM/DD/YYYY' to signal user to enter an date, explicitly. - Added Part 1 homework

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,42 @@
 // from data.js
-var tableData = data;
+const tableData = data;
 
-// YOUR CODE HERE!
+function handleForm() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    // parse entered date filter string
+    const datetime = d3.select('#datetime').property('value');
+    console.log(`datetime is ${datetime}`);   
+
+    // filter data based on date
+    filteredTableData = tableData.filter(row => {
+        return row.datetime === datetime;
+    });
+    // console.log(filteredTableData);
+
+    // Locate tbody tag and store into a variable
+    const tbody = d3.select('tbody');
+
+    // Append rows populated with filtered data
+    filteredTableData.forEach(row => {
+        // console.log(row);
+        const tr = tbody.append('tr');
+        // Assuming the returned value arrays are in the same order as stored.
+        // We don't need key names to display
+        Object.values(row).forEach((value) => {
+            //console.log(value);
+            tr.append('td').text(value);
+        });
+    });
+}
+
+// Locate 'filter table' button and form.
+const button = d3.select('#filter-btn');
+const form = d3.select('form');
+
+// Add event handler. 
+button.on('click', handleForm);
+form.on('submit', handleForm);
+
+
